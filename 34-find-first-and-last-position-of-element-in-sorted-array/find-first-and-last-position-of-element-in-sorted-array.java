9ba2abd9af94996eaa24c03cffce3adf6e744e98
snehalgavhane33 <snehalgavhane33@gmail.com>
2026-08-11 21:11:45 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (32.34%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,44 +1,94 @@
 //CODE 1
-class Solution {
-    public static int lowerBound(int arr[], int target){
-        int n =arr.length;
-        int low=0;
-        int high =n-1;
-        int ans =n;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(arr[mid]>=target){
-                ans= mid;
-                high = mid-1;
-            }else{
-                low= mid+1;
-            }
-        }
-        return ans;
-    }
-    public static int upperBound(int arr[], int target){
+// class Solution {
+//     public static int lowerBound(int arr[], int target){
+//         int n =arr.length;
+//         int low=0;
+//         int high =n-1;
+//         int ans =n;
+//         while(low<=high){
+//             int mid=(low+high)/2;
+//             if(arr[mid]>=target){
+//                 ans= mid;
+//                 high = mid-1;
+//             }else{
+//                 low= mid+1;
+//             }
+//         }
+//         return ans;
+//     }
+//     public static int upperBound(int arr[], int target){
+//         int n = arr.length;
+//         int low=0;
+//         int high =n-1;
+//         int ans =n;
+//         while(low<=high){
+//             int mid=(low+high)/2;
+//             //just comparison operator difference
+//             if(arr[mid]>target){
+//                 ans= mid;
+//                 high = mid-1;
+//             }else{
+//                 low= mid+1;
+//             }
+//         }
+//         return ans;
+//     }
+//     public int[] searchRange(int[] arr, int target) {
+//         int n=arr.length;
+//         int lb = lowerBound(arr, target);
+//         if(lb==n || arr[lb] != target){
+//             return new int[]{-1,-1};
+//         } 
+//         return new int[] {lb, upperBound(arr, target)-1};
+//     }
+// }
+
+
+//CODE 2
+class Solution{
+    public static int firstOccurance(int arr[], int target){
         int n = arr.length;
         int low=0;
-        int high =n-1;
-        int ans =n;
+        int high = n-1;
+        int first =-1;
         while(low<=high){
             int mid=(low+high)/2;
-            //just comparison operator difference
-            if(arr[mid]>target){
-                ans= mid;
+            if(arr[mid]==target){
+                first =mid;
                 high = mid-1;
-            }else{
+            }else if(arr[mid]<target){
                 low= mid+1;
+            }else{
+                high=mid-1;
             }
         }
-        return ans;
+        return first;
     }
-    public int[] searchRange(int[] arr, int target) {
-        int n=arr.length;
-        int lb = lowerBound(arr, target);
-        if(lb==n || arr[lb] != target){
+    public static int lastOccurance(int arr[], int target){
+        int n = arr.length;
+        int low=0;
+        int high = n-1;
+        int last =-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]==target){
+                last =mid;
+                low = mid+1;
+            }else if(arr[mid]<target){
+                low= mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return last;
+    }
+
+     public int[] searchRange(int[] arr, int target){
+        int first = firstOccurance(arr, target);
+        if(first == -1){
             return new int[]{-1,-1};
-        } 
-        return new int[] {lb, upperBound(arr, target)-1};
-    }
+        }
+        int last= lastOccurance(arr, target);
+        return new int[]{first,last};
+     }
 }
